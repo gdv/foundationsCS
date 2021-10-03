e90b03d551292cda7098fa9c4dc539b7fece0e12
s.ciccolella <s.ciccolella@campus.unimib.it>
2021-10-03 18:50:43 +0200

    add solutions

--- sql-01a-sfw.sql
+++ sql-01a-sfw.sql
@@ -1,1 +1,8 @@
--- test
+1. SELECT * FROM Customers WHERE Country == 'Italy' OR Country == 'France' OR Country == 'Austria';
+2. SELECT * FROM Customers WHERE Address LIKE '%rue%';
+3. SELECT * FROM Customers WHERE length(City) > 5;
+4. SELECT * FROM Products WHERE Id > 20;
+5. SELECT CustomerId FROM Orders GROUP BY CustomerId;
+6. SELECT CustomerId, count(*) FROM Orders GROUP BY CustomerId HAVING count(*) > 150;
+7. SELECT OrderId, sum(Quantity) FROM OrderDetails GROUP BY OrderId;
+8. SELECT OrderId, sum(Quantity), avg(Discount) FROM OrderDetails GROUP BY OrderId HAVING avg(Discount) > 0;
